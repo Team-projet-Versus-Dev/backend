158a8d0f9a2f78ea5cbeeac10d18b7b9a58430c4
Lonny <lonnymi971@gmail.com>
2025-11-26 11:30:59 +0100

    mise à jour vote + ranking + app.module pour intégrer le module duel

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AnimeModule } from './anime/anime.module';
+import { DuelModule } from './duel/duel.module';
+import { VoteModule } from './vote/vote.module';
 
 @Module({
-  imports: [],
+  imports: [AnimeModule, DuelModule, VoteModule],
   controllers: [AppController],
   providers: [AppService],
 })
